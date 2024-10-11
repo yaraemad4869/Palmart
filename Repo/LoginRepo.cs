49ea9037e1295bbc0109e19a5b433f9a81a66513
yaraemad4869 <143430513+yaraemad4869@users.noreply.github.com>
2024-10-11 16:12:40 +0300

    Add files via upload

---
+++ Repo/LoginRepo.cs
@@ -0,0 +1,19 @@
+﻿using Palmart.Data;
+using Palmart.IRepo;
+using Palmart.Models;
+
+namespace Palmart.Repo
+{
+	public class LoginRepo : ILoginRepo
+	{
+		private readonly AppDbContext _db;
+        public LoginRepo(AppDbContext db)
+        {
+            _db = db;
+        }
+		public User GetByEmail(string email) {
+			var user = _db.Users.FirstOrDefault(x => x.Email.ToLower() == email.ToLower());
+			return user ; 
+		}
+	}
+}
